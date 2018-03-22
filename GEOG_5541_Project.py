fd9f6821f953807848d2de556ffdbcca54bff9a6
laurenstrug <37091643+laurenstrug@users.noreply.github.com>
2018-03-22 17:05:28 -0500

    starting

---
+++ GEOG_5541_Project.py
@@ -0,0 +1,32 @@
+
+
+import rasterio
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+
+
+file = "Downloads/MN_lidar_tile_-120-114_dem.tif" #Locates file
+lidar_tile_120 = rasterio.open(file) #Opens file
+
+import os.path
+os.path.isfile(file)
+
+lidar_tile_120.profile #Gets metadata
+
+lidar_tile_120.indexes #Checks number of bands
+
+lidar_tile_120.read(1) #Reads first band
+
+def hillshade(array, azimuth, angle_altitude): # Based on ESRI hillshade
+    x, y = gradient(array) # http://geologyandpython.com/dem-processing.html
+    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
+    aspect = np.arctan2(-x, y)
+    azimuthrad = azimuth*np.pi / 180.
+    altituderad = angle_altitude*np.pi / 180.
+
+    shaded = np.sin(altituderad) * np.sin(slope) \
+    + np.cos(altituderad) * np.cos(slope) \
+    * np.cos(azimuthrad - aspect)
+    return 255*(shaded + 1)/2
