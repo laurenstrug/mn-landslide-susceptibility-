a758a1753212d98fbef068849f8825268d049dc6
laurenstrug <37091643+laurenstrug@users.noreply.github.com>
2018-04-05 12:36:58 -0500

    added functions, started loop

--- GEOG_5541_Project.py
+++ GEOG_5541_Project.py
@@ -3,12 +3,13 @@
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import geopandas as gpd
+import glob
 
 
 file = "Downloads/MN_lidar_tile_-120-114_dem.tif" #Locates file
 lidar_tile_120 = rasterio.open(file) #Opens file
+
+folder = "Downloads/Lidar tiles" #Locates folder with three tiles
 
 import os.path
 os.path.isfile(file)
@@ -31,6 +32,15 @@ def hillshade(array, azimuth, angle_altitude): # Based on ESRI hillshade
     * np.cos(azimuthrad - aspect)
     return 255*(shaded + 1)/2
 
+def slope(array)
+    x, y = np.gradient(array)
+    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
+    return slope
+
+def aspect(array)
+    x, y = np.gradient(array)
+    aspect = np.arctan2(-x, y)
+    return aspect
 
 with rasterio.open(file) as src:
     band1 = src.read(1)
@@ -38,3 +48,10 @@ with rasterio.open(file) as src:
 hshade_array = hillshade(band1, 45, 45)
 plt.imshow(hshade_array, cmap = 'Greys')
 plt.show()
+
+for filename in folder.glob("*.tif"):
+    with rasterio.open(filename) as src:
+        band1 = src.read(1)
+    hillshade(band1, 45, 45)
+    slope(band1)
+    aspect(band1)
