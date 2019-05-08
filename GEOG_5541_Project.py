2fd70df04006b89bba1c8d629349d7b28e2a9579
Lauren Strug <37091643+laurenstrug@users.noreply.github.com>
2019-05-07 22:01:34 -0500

    Final Script GIS 5572
    This is the final update for the script created the GIS 5572 which uses arcpy to output hillshade, slope, and aspect for multiple rasters.

--- GEOG_5541_Project.py
+++ GEOG_5541_Project.py
@@ -1,89 +1,31 @@
+import arcpy
+#Defining input and output parameters
+inputras = arcpy.GetParameter(0)
+outputhill = arcpy.GetParameterAsText(8)
+outputslope = arcpy.GetParameterAsText(9)
+outputaspect = arcpy.GetParameterAsText(10)
 
+#Hillshade parameters
+azimuth = arcpy.GetParameter(1)
+altitude = arcpy.GetParameter(2)
+modelShadows = arcpy.GetParameter(3)
+zFactor = arcpy.GetParameter(6)
 
-import rasterio
-import numpy as np
-import matplotlib.pyplot as plt
-import gdal 
-#TESTING 
+#Slope and Aspect parameters
+outMeasurement = arcpy.GetParameter(4)
+method = arcpy.GetParameter(5)
+zUnit = arcpy.GetParameter(7)
 
-file = "Downloads/MN_lidar_tile_-120-114_dem.tif" #Locates file
-lidar_tile_120 = rasterio.open(file) #Opens file
+count = 0
+for ras in inputras:
+	#Execute hillshade with set parameters for input
+	arcpy.HillShade_3d(ras, outputhill + str(count), azimuth, altitude, modelShadows, zFactor)
 
-folder = "Downloads/Lidar tiles" #Locates folder with three tiles
+	#Execute Slope with set parameters
+	arcpy.Slope_3d(ras, outputslope + str(count), outMeasurement, zFactor, method, zUnit)
 
-import os.path
-os.path.isfile(file)
-
-lidar_tile_120.profile #Gets metadata
-
-lidar_tile_120.indexes #Checks number of bands
-
-lidar_tile_120.read(1) #Reads first band
-
-def hillshade(array, azimuth, angle_altitude): # Based on ESRI hillshade
-    x, y = np.gradient(array) # http://geologyandpython.com/dem-processing.html
-    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
-    aspect = np.arctan2(-x, y)
-    azimuthrad = azimuth*np.pi / 180.
-    altituderad = angle_altitude*np.pi / 180.
-
-    shaded = np.sin(altituderad) * np.sin(slope) \
-    + np.cos(altituderad) * np.cos(slope) \
-    * np.cos(azimuthrad - aspect)
-    return 255*(shaded + 1)/2
-
-def slope(array):
-    x, y = np.gradient(array)
-    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
-    return slope
-
-def aspect(array):
-    x, y = np.gradient(array)
-    aspect = np.arctan2(-x, y)
-    return aspect
-
-with rasterio.open(file) as src:
-    band1 = src.read(1)
-    
-hshade_array = hillshade(band1, 45, 45)
-plt.imshow(hshade_array, cmap = 'Greys')
-plt.show()
-
-#empyt 4 geodatabases: one for hillshade45, hillshade315, slope, and aspect
-import os 
-import glob
-import gdal 
-
-path = 'C:\\Users\\Lauren\\Downloads\\Lidar tiles' #Locates folder with multiple tif files stored in it
-
-def create_tif(outfilename, arr_out, rows, cols):
-    driver = gdal.GetDriverByName("GTiff")
-    outdata = driver.Create(outfilename, rows, cols, 1, gdal.GDT_UInt32)
-    outband = outdata.GetRasterBand(1)
-    outdata.SetProjection(ds.GetProjection())#sets same projection as input
-    outband.WriteArray(arr_out)
-    outdata.FlushCache() #saves to disk
-
-    
-for filename in glob.glob(os.path.join(path, '*.tif')): #Loop through folder with files ending in .tif
-    ds = gdal.Open(filename) #Open file using gdal
-    band = ds.GetRasterBand(1)
-    band1 = band.ReadAsArray() #Reads band 1 as an array
-
-    shapelist = band1.shape #Gets the size of the array with numpy
-    cols = shapelist[0] #Stores the width of the raster
-    rows = shapelist[1] #stores the height of the raster
-    
-    input_functions = [hillshade(band1, 45, 45), hillshade(band1, 315, 45), slope(band1), aspect(band1)] #List of functions
-    folder = ['hillshade45','hillshade315','slope','aspect'] #List of folders where the output of functions will be stored
-    endings = ['_shd45.tif','_shd315.tif','slp.tif','apt.tif'] #List of endings for each folder type 
-    
-    for i in range(0,4): #Loops through four times
-        folderpath = path + "\\" + folder[i] + "\\" + filename[:3] + endings[i] #output folder is equal to the lidar tiles folder 
-        create_tif(folderpath, input_functions[i], rows, cols)                  #plus the folder we are iterating through, plus
-                                                                                #the filename minus the last 4 characters, plus
-                                                                                #the file ending. 
-    outdata = None #important to close the files
-    band = None
-    ds = None 
+	#Execute Aspect with set parameters
+	arcpy.Aspect_3d(ras, outputaspect + str(count), method, zUnit)
+	
+	count += 1
     
--- GIS5572Project.py
+++ GIS5572Project.py
@@ -1,27 +1,44 @@
 import arcpy
 
-inputras = arcpy.GetParameter(0)
-outputhill = arcpy.GetParameter(1)
-outputslope = arcpy.GetParameter(2)
-outputaspect = arcpy.GetParameter(3)
+class ToolValidator(object):
+    """Class for validating a tool's parameter values and controlling
+    the behavior of the tool's dialog."""
 
-#Hillshade parameters
-azimuth = 180
-altitude = 75
-modelShadows = "SHADOWS"
-zFactor = 0.5
+    def __init__(self):
+        """Setup arcpy and the list of tool parameters.""" 
+        self.params = arcpy.GetParameterInfo()
 
-#Slope and Aspect parameters
-outMeasurement = "DEGREE"
-zFactorslope = ""
-method = "GEODESIC"
-zUnit = "FOOT"
+    def initializeParameters(self): 
+        """Refine the properties of a tool's parameters. This method is 
+        called when the tool is opened."""
 
-#Execute hillshade with set parameters for input
-arcpy.HillShade_3d(inputras, outputhill, azimuth, altitude, modelShadows, zFactor)
+    def updateParameters(self):
+        """Modify the values and properties of parameters before internal
+        validation is performed. This method is called whenever a parameter
+        has been changed."""
+        # Update the value list filter in the second parameter based on the 
+        #   shape type in the first parameter
+        #
+        stringFilter = self.params[1].filter
 
-#Execute Slope with set parameters
-arcpy.Slope_3d(inputras, outputslope, outMeasurement, zFactorslope, method, zUnit)
+        if self.params[0].valueAsText:
+            shapetype = arcpy.Describe(self.params[0]).shapeType.lower()
+            if shapetype == "point" or shapetype == "multipoint":
+                stringFilter.list = ["RED", "GREEN", "BLUE"]
+            elif shapetype == "polygon":
+                stringFilter.list = ["WHITE", "GRAY", "BLACK"]
+            else:
+                stringFilter.list = ["ORANGE", "INDIGO", "VIOLET"]
+        else:
+            stringFilter.list = ["RED", "GREEN", "BLUE"]
 
-#Execute Aspect with set parameters
-arcpy.Aspect_3d(inputras, outputaspect, method, zUnit)
+        # If the user hasn't changed the keyword value, set it to the default value
+        #  (first value in the value list filter).
+        #
+        if not self.params[1].altered:
+            self.params[1].value = stringFilter.list[0]
+            
+        return
+    def updateMessages(self):
+        """Modify the messages created by internal validation for each tool
+        parameter. This method is called after internal validation."""
